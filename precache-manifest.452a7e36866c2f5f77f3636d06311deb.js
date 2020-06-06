d3933f593baf4b733b908883456f56c49cb46a9e
DanArcherOnline <danarcheronline@gmail.com>
2020-06-06 16:12:31 +0900

    Updates

---
+++ precache-manifest.452a7e36866c2f5f77f3636d06311deb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4af3cb7bbca3a820f4070aad9c4c7b2",
+    "url": "/cats-rolodex/index.html"
+  },
+  {
+    "revision": "a6acab532117222cb6b2",
+    "url": "/cats-rolodex/static/css/main.554067df.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/cats-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cats-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6acab532117222cb6b2",
+    "url": "/cats-rolodex/static/js/main.c0e4d966.chunk.js"
+  },
+  {
+    "revision": "f3619f5b32a2d9ecc6f2",
+    "url": "/cats-rolodex/static/js/runtime-main.f5fe2f9e.js"
+  }
+]);
